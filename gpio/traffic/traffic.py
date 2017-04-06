6f62c41d65f07bc2e0772e26d289823d1a16e03a
Ericmas001 <ericmas001@gmail.com>
2017-04-05 20:57:48 -0400

    Refactoring traffic

--- gpio/traffic/traffic.py
+++ gpio/traffic/traffic.py
@@ -3,56 +3,59 @@ import time
 import datetime
 import RPi.GPIO as GPIO
 
-
 RED = 11
 YELLOW = 7
 GREEN = 13
 
-def red():
-    return pRed
+class Led(object):
+    def __init__(self, pin, name):
+        self.pin = pin
+        self.name = name
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.gpio = GPIO.PWM(self.pin, 50)
+        self.gpio.start(0)
 
-def yellow():
-    return pYellow
+    def intensity(self, value):
+        self.gpio.ChangeDutyCycle(value)
 
-def green():
-    return pGreen
+    def on(self):
+        self.intensity(100)
 
-def pulse(p, nb):
+    def off(self):
+        self.intensity(0)
+    
+    def kill(self):
+        self.gpio.stop()
+        GPIO.output(self.pin, False)
+
+def pulse(led, nb):
     print("    -> Will blink {0} times".format(nb))
     for k in range(nb):
         time.sleep(0.1)
         for i in range(100):
-            p.ChangeDutyCycle(i)
+            led.intensity(i)
             time.sleep(0.001)
         time.sleep(0.25)
         for i in range(100):
-            p.ChangeDutyCycle(100 - (i))
+            led.intensity(100 - (i))
             time.sleep(0.001)
-        p.ChangeDutyCycle(0)
+        led.off()
 
 
 def Its(led, nbSecAfterFlash, nbFlash=0):
-    print("[{1}] Its time for {0}".format(led.__name__,datetime.datetime.now()))
+    print("[{1}] Its time for {0}".format(led.name,datetime.datetime.now()))
     if nbFlash > 0:
-        pulse(led(), nbFlash)
-    led().ChangeDutyCycle(100)
+        pulse(led, nbFlash)
+    led.on()
     time.sleep(nbSecAfterFlash)
-    led().ChangeDutyCycle(0)
+    led.off()
 
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(RED, GPIO.OUT)
-pRed = GPIO.PWM(RED, 50)
-pRed.start(0)
-
-GPIO.setup(YELLOW, GPIO.OUT)
-pYellow = GPIO.PWM(YELLOW, 50)
-pYellow.start(0)
-
-GPIO.setup(GREEN, GPIO.OUT)
-pGreen = GPIO.PWM(GREEN, 50)
-pGreen.start(0)
+red = Led(RED,"red")
+yellow = Led(YELLOW,"yellow")
+green = Led(GREEN,"green")
 
 try:
     nb = 0
@@ -69,14 +72,9 @@ except KeyboardInterrupt:
     print("Interrupted by Keyboard")
     
 
-pRed.stop()
-GPIO.output(RED, False)
-
-pYellow.stop()
-GPIO.output(YELLOW, False)
-
-pGreen.stop()
-GPIO.output(GREEN, False)
+red.kill()
+yellow.kill()
+green.kill()
 
 GPIO.cleanup()
 print("Time to stop")
--- gpio/traffic/traffic2.py
+++ gpio/traffic/traffic2.py
@@ -3,102 +3,58 @@ import time
 import datetime
 import RPi.GPIO as GPIO
 
-def red():
-    return 11
+RED = 11
+YELLOW = 7
+GREEN = 13
+BUTTON = 18
 
-def yellow():
-    return 7
+class Led(object):
+    def __init__(self, pin, name):
+        self.pin = pin
+        self.name = name
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.gpio = GPIO.PWM(self.pin, 50)
+        self.gpio.start(0)
 
-def green():
-    return 13
+    def intensity(self, value):
+        self.gpio.ChangeDutyCycle(value)
 
-def pulse(p, nb):
-    print("    -> Will blink {0} times".format(nb))
-    for k in range(nb):
-        time.sleep(0.1)
-        for i in range(100):
-            p.ChangeDutyCycle(i)
-            time.sleep(0.001)
-        time.sleep(0.25)
-        for i in range(100):
-            p.ChangeDutyCycle(100 - (i))
-            time.sleep(0.001)
-        p.ChangeDutyCycle(0)
+    def on(self):
+        self.intensity(100)
 
-
-def Its(led, nbSecAfterFlash, nbFlash=0):
-    print("[{1}] Its time for {0}".format(led.__name__,datetime.datetime.now()))
-    p = GPIO.PWM(led(), 50)
-    p.start(0)
-    if nbFlash > 0:
-        pulse(p, nbFlash)
-    p.ChangeDutyCycle(100)
-    time.sleep(nbSecAfterFlash)
-    p.stop()
+    def off(self):
+        self.intensity(0)
+    
+    def kill(self):
+        self.gpio.stop()
+        GPIO.output(self.pin, False)
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(red(), GPIO.OUT)
-pRed =  GPIO.PWM(red(), 50)
-pRed.start(0)
-
-GPIO.setup(yellow(), GPIO.OUT)
-pYellow =  GPIO.PWM(yellow(), 50)
-pYellow.start(0)
-
-GPIO.setup(green(), GPIO.OUT)
-pGreen =  GPIO.PWM(green(), 50)
-pGreen.start(0)
-
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+red = Led(RED,"red")
+yellow = Led(YELLOW,"yellow")
+green = Led(GREEN,"green")
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
     nb = 0
-    state = 0
+    state = 1
+    leds = [red,yellow,green]
     while True:
-        input_state = GPIO.input(18)
+        input_state = GPIO.input(BUTTON)
         if input_state == False:
             state = (state + 1) % 6
-            print("[{1}] {2} Button Pressed ! State: {0}".format(state,datetime.datetime.now(),nb))
-            if state % 2 == 0:
-                print("[{0}] {1} Its time to stop".format(datetime.datetime.now(),nb))
-                pRed.ChangeDutyCycle(0)
-                pYellow.ChangeDutyCycle(0)
-                pGreen.ChangeDutyCycle(0)
-            if state == 1:
-                print("[{1}] {2} Its time for {0}".format("red",datetime.datetime.now(),nb))
-                pRed.ChangeDutyCycle(100)
-            if state == 3:
-                pass
-                print("[{1}] {2} Its time for {0}".format("green",datetime.datetime.now(),nb))
-                pYellow.ChangeDutyCycle(100)
-            if state == 5:
-                print("[{1}] {2} Its time for {0}".format("yellow",datetime.datetime.now(),nb))
-                pGreen.ChangeDutyCycle(100)
-            # if(nb < 250):
-            time.sleep(0.025)
-            # else:
-            #     time.sleep(0.5)
+            print("[{1}] {2} Button Pressed ! State: {0} Button: {3} Becoming {4}".format(state,datetime.datetime.now(),nb, leds[int(state/2)].name, "on" if (state % 2) == 0 else "off"))
+            leds[int(state/2)].intensity(((state+1) % 2)*100)
+            time.sleep(0.25)
             nb = nb + 1
-    #    nb = nb + 1
-    #    print("[{1}] Cycle #{0}".format(nb,datetime.datetime.now()))
-    #    Its(red, 1)
-    #    time.sleep(0.5)
-    #    Its(green, 1, 7)
-    #    Its(yellow, 1)
-    #    print("[{0}] End Of Cycle".format(datetime.datetime.now()))
-    #    print("============================")
 except KeyboardInterrupt:
     print("Interrupted by Keyboard")
     
-pRed.start(0)
-GPIO.output(red(), False)
 
-pYellow.start(0)
-GPIO.output(green(), False)
-
-pGreen.start(0)
-GPIO.output(yellow(), False)
+red.kill()
+yellow.kill()
+green.kill()
 
 GPIO.cleanup()
 print("Time to stop")
